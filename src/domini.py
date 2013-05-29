gnank/gnank

commit 14f0ab919aa0f227131a2592baf115a3a24af6b0

Fixed linting errors and unused variables

src/domini.py
@@ -18,12 +18,12 @@
 
 
 import dades
-from dades import ErrorDades, ErrorOpcions
 from itertools import chain
 
 _carrera = ""
 _assigs = {}
 _horaris = set()
+
 
 def obre(fitxer):
     global _carrera
@@ -34,10 +34,12 @@ def obre(fitxer):
     for horari in horaris:
         afegeix_horari_preferit(horari)
 
+
 def desa(fitxer):
     iters = [assig.tuples_classes() for assig in _assigs.itervalues()]
     tuples = chain(*iters)
     dades.desa(fitxer, _carrera, sorted(tuples), sorted(_horaris))
+
 
 def actualitza():
     global _horaris, _carrera
@@ -50,40 +52,51 @@ def actualitza():
     horaris_valids.discard(tuple())
     _horaris = horaris_valids
 
+
 def grups_disponibles():
     grups_a = lambda a: [(g.nom) for g in sorted(a.grups_disponibles())]
     return [(a.nom, grups_a(a)) for a in sorted(_assigs.itervalues())]
+
 
 def grups_disponibles_mati():
     grups_a = lambda a: [(g.nom) for g in sorted(a.grups_disponibles_mati())]
     return [(a.nom, grups_a(a)) for a in sorted(_assigs.itervalues())]
 
+
 def grups_disponibles_tarda():
     grups_a = lambda a: [(g.nom) for g in sorted(a.grups_disponibles_tarda())]
     return [(a.nom, grups_a(a)) for a in sorted(_assigs.itervalues())]
 
+
 def horaris_preferits():
     return [Horari(g) for g in _horaris]
+
 
 def es_horari_preferit(horari):
     return tuple(sorted(horari)) in _horaris
 
+
 def afegeix_horari_preferit(horari):
     _horaris.add(tuple(sorted(horari)))
 
+
 def elimina_horari_preferit(horari):
     _horaris.discard(tuple(sorted(horari)))
+
 
 def _afegeix_classes(classes):
     for assig, grup, dia, hora, tipus, aula in classes:
         classe = Classe(assig, grup, dia, hora, tipus, aula)
         _assigs.setdefault(assig, Assig(assig)).afegeix_classe(classe)
 
+
 def _hores_classes(assig, grup):
-    return  _assigs[assig].grup(grup).hores_classes()
+    return _assigs[assig].grup(grup).hores_classes()
+
 
 def _classes_dia_hora(assig, grup, dia, hora):
     return _assigs[assig].grup(grup).classes(dia, hora)
+
 
 def _horari_valid(horari):
     horari_valid = []
@@ -93,12 +106,15 @@ def _horari_valid(horari):
             horari_valid.append((nom_assig, grup))
     return tuple(horari_valid)
 
+
 def actualitza_carrera(carrera):
     global _carrera
     _carrera = carrera
 
+
 def obte_carrera():
     return _carrera
+
 
 class Classe(object):
 
@@ -149,7 +165,7 @@ class Grup(object):
     def __init__(self, nom, supergrup=None):
         self.nom = nom
         self._supergrup = supergrup
-        self._classes = {} # { (dia, hora): classes }
+        self._classes = {}  # { (dia, hora): classes }
         self._mati = True
         self._tarda = True
 
@@ -252,7 +268,7 @@ class Horari(object):
 
     def __init__(self, grups=[]):
         self._tupla = tuple(sorted(grups))
-        self._grups = {} # { assig: grups }
+        self._grups = {}  # { assig: grups }
         for assig, grup in grups:
             self._grups.setdefault(assig, set()).add(grup)
         self._calcula_estadistiques()
@@ -286,7 +302,7 @@ class Horari(object):
         return iter(self._tupla)
 
     def _calcula_estadistiques(self):
-        assigs_dh = {} # { (dia, hora) : assig }
+        assigs_dh = {}  # { (dia, hora) : assig }
 
         for assig, grup in self._tupla:
             for dh in _hores_classes(assig, grup):
@@ -431,7 +447,6 @@ class Cerca(object):
         return solucio
 
     def _combinacio(self):
-        grup = self._grup
         c = 0
         n = 1
         for i in range(len(self._grup) - 1, -1, -1):
@@ -452,7 +467,6 @@ class Cerca(object):
 
         nivell = self._nivell
         return self._grup[nivell] <= len(self._grups[nivell])
-
 
     def _no_solucionable(self):
         """Indica si és impossible trobar noves solucions des del node."""
@@ -522,4 +536,3 @@ class Cerca(object):
                 n_solap -= 1
             classes[c] = s
         self._n_solap = n_solap
-

src/config.py
@@ -16,26 +16,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys, logging
+import logging
+import os
 from os.path import abspath, dirname, join
+import re
+import sys
 
 NOM = "Gnank"
 VERSIO = "3.2"
-DESCRIPCIO =  "Cercador d'horaris de la FIB"
-URL_WEB =  "http://lafarga.cpl.upc.edu/projectes/gnank-reloaded"
+DESCRIPCIO = "Cercador d'horaris de la FIB"
+URL_WEB = "http://lafarga.cpl.upc.edu/projectes/gnank-reloaded"
 COPYRIGHT = "Copyright © 2006, 2007  Albert Gasset Romo"
 AUTOR = "Marc Cornellà"
 EMAIL_AUTOR = "marc.cornella@est.fib.upc.edu"
 LLICENCIA = "GPL"
 AUTORS = [
-	"Desenvolupador actual:",
-	"Marc Cornellà <marc.cornella@est.fib.upc.edu>",
-	"",
-	"Desenvolupador principal:",
-	"Albert Gasset Romo <albert.gasset@gmail.com>",
-	"",
-	"Paquet Debian i pedaços:",
-	"Jordà Polo <jorda@ettin.org>"
+    "Desenvolupador actual:",
+    "Marc Cornellà <marc.cornella@est.fib.upc.edu>",
+    "",
+    "Desenvolupador principal:",
+    "Albert Gasset Romo <albert.gasset@gmail.com>",
+    "",
+    "Paquet Debian i pedaços:",
+    "Jordà Polo <jorda@ettin.org>"
 ]
 INFO_LLICENCIA = """\
 This program is free software: you can redistribute it and/or modify
@@ -53,40 +56,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 if sys.platform == "win32":
-	import _winreg
-	def regval(key, subkey, name):
-		key = _winreg.OpenKey(key, subkey)
-		try:
-			ret = _winreg.QueryValueEx(key, name)
-		except WindowsError:
-			return None
-		key.Close()
-		if ret[1] == _winreg.REG_EXPAND_SZ:
-			substenv = lambda m: os.environ.get(m.group(1), m.group(0))
-			return re.compile(r'%([^|<>=^%]+)%').sub(substenv,ret[0])
-		return ret[0]
-	DIR_USUARI = join(regval(_winreg.HKEY_CURRENT_USER,
-			"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders",
-			"AppData"), "Gnank")
+    import _winreg
+
+    def regval(key, subkey, name):
+        key = _winreg.OpenKey(key, subkey)
+        try:
+            ret = _winreg.QueryValueEx(key, name)
+        except WindowsError:
+            return None
+        key.Close()
+        if ret[1] == _winreg.REG_EXPAND_SZ:
+            substenv = lambda m: os.environ.get(m.group(1), m.group(0))
+            return re.compile(r'%([^|<>=^%]+)%').sub(substenv, ret[0])
+        return ret[0]
+    DIR_USUARI = join(regval(_winreg.HKEY_CURRENT_USER,
+            "Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders",
+            "AppData"), "Gnank")
 else:
-	DIR_USUARI = join(os.environ['HOME'], ".gnank")
+    DIR_USUARI = join(os.environ['HOME'], ".gnank")
 
 
 HORARIS_USUARI = join(DIR_USUARI, "horaris")
 REGISTRE_USUARI = join(DIR_USUARI, "registre")
 
+
 def crea_dir_usuari():
-	try:
-		if not os.path.exists(DIR_USUARI):
-			os.makedirs(DIR_USUARI)
-	except OSError:
-		pass
+    try:
+        if not os.path.exists(DIR_USUARI):
+            os.makedirs(DIR_USUARI)
+    except OSError:
+        pass
 
 
 def cami(fitxer):
-	cami_fitxer = join(abspath(dirname(sys.argv[0])), fitxer)
-	if os.access(cami_fitxer, os.F_OK):
-		return cami_fitxer
-	logging.warning("No es pot accedir al fitxer: %s", fitxer)
-	return None
-
+    cami_fitxer = join(abspath(dirname(sys.argv[0])), fitxer)
+    if os.access(cami_fitxer, os.F_OK):
+        return cami_fitxer
+    logging.warning("No es pot accedir al fitxer: %s", fitxer)
+    return None

src/dades.py
@@ -25,8 +25,10 @@ URL_CLASSES = "https://raco.fib.upc.edu/api/horaris/horari-assignatures.txt"
 _ER_CLASSE = re.compile("[^\s]+\s+[^\s]+\s+[0-9]+\s+[0-9]+(:00)?\s+[^\s]+\s+[^\s]+")
 _ER_HORARI = re.compile("[^\s]+\s+[^\s]+(\s+[^\s]+\s+[^\s]+)*")
 
+
 class ErrorOpcions(Exception):
-	pass
+    pass
+
 
 class ErrorDades(Exception):
     pass
@@ -53,7 +55,7 @@ def obre(fitxer):
                 if not _ER_HORARI.match(linia):
                     raise ErrorDades
                 linia = linia.split()
-                horaris.append(zip(linia[::2],linia[1::2]))
+                horaris.append(zip(linia[::2], linia[1::2]))
             else:
                 if not _ER_CLASSE.match(linia):
                     raise ErrorDades
@@ -66,7 +68,7 @@ def obre(fitxer):
     return carrera, classes, horaris
 
 
-def desa(fitxer, carrera, classes, horaris = None):
+def desa(fitxer, carrera, classes, horaris=None):
     try:
         f = file(fitxer, "wb")
         f.write(carrera)
@@ -77,7 +79,7 @@ def desa(fitxer, carrera, classes, horaris = None):
         if horaris is not None:
             f.write(";\n")
             for horari in horaris:
-                f.write(" ".join([a+" "+g for a,g in horari]))
+                f.write(" ".join([a + " " + g for a, g in horari]))
                 f.write("\n")
     except IOError:
         raise ErrorDades
@@ -86,13 +88,14 @@ def desa(fitxer, carrera, classes, horaris = None):
 def obre_http(carrera):
     classes = []
     try:
-        if carrera == "": raise ErrorOpcions
+        if carrera == "":
+            raise ErrorOpcions
 
         # obtenim les assignatures
         assigs = [a.strip() for a in urlopen(URL_ASSIGS + carrera)]
 
         # borrem l'assignatura GRAU-EXAMENS en cas que hi sigui
-        assigs = filter (lambda a: a != "GRAU-EXAMENS", assigs)
+        assigs = filter(lambda a: a != "GRAU-EXAMENS", assigs)
 
         if assigs:
             params = "?assignatures=" + "&assignatures=".join(assigs)

src/gnank.py
@@ -18,7 +18,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import sys, os
+import sys
+import os
 from os.path import dirname, join
 import logging
 from traceback import format_exception

src/gui.py
@@ -16,20 +16,20 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 import pygtk
 pygtk.require('2.0')
-import gtk, gobject, pango
-import cairo
-import domini, config
-from domini import Classe, Horari, Cerca, ErrorDades, ErrorOpcions
-from string import lower
-from time import clock
-from thread import start_new_thread
+import gtk
+import gobject
+import pango
 import webbrowser
 import logging
+import config
+import domini
+from domini import Classe, Horari, Cerca, ErrorDades, ErrorOpcions
+from thread import start_new_thread
 
 gtk_versio_2_10 = gtk.check_version(2, 10, 0) is None
+
 
 def inicia():
     if not gtk_versio_2_10:
@@ -106,7 +106,7 @@ class Finestra(gtk.Window):
         area_finestra.pack_start(area_treball, expand=True, fill=True)
 
         arbre = ArbreGrups()
-        area_treball.pack1(arbre, resize=False, shrink=False )
+        area_treball.pack1(arbre, resize=False, shrink=False)
 
         llista = LlistaHoraris(accions)
         area_llista = gtk.ScrolledWindow()
@@ -156,7 +156,6 @@ class Accions(gtk.ActionGroup):
         'dades-actualitzades': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
         'neteja': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
     }
-
 
     def __init__(self, finestra):
         """Inicialitza les accions."""
@@ -285,16 +284,16 @@ class TriaCarrera(gtk.HBox):
     DESC, CODI = range(0, 2)
 
     _codiscarrera = [ \
-        (u"Grau","GRAU"), \
-        (u"Eng. Inf. Superior","EI03"), \
-        (u"Eng. Tèc. Sistemes","ETS03"), \
-        (u"Eng. Tèc. Gestió","ETG03"), \
-        (u"M. d'Arquitectura de Computadors, Xarxes i Sistemes","CANS"), \
-        (u"M. d'Inteligència Artificial","MIA"), \
-        (u"M. de Computació","MC"), \
-        (u"M. de Tecnologies de la Informació","MTI"), \
-        (u"M. en Enginyeria Informàtica","MEI"), \
-        (u"M. Erasmus de Computació Distribuïda","EMDC")
+        (u"Grau", "GRAU"), \
+        (u"Eng. Inf. Superior", "EI03"), \
+        (u"Eng. Tèc. Sistemes", "ETS03"), \
+        (u"Eng. Tèc. Gestió", "ETG03"), \
+        (u"M. d'Arquitectura de Computadors, Xarxes i Sistemes", "CANS"), \
+        (u"M. d'Inteligència Artificial", "MIA"), \
+        (u"M. de Computació", "MC"), \
+        (u"M. de Tecnologies de la Informació", "MTI"), \
+        (u"M. en Enginyeria Informàtica", "MEI"), \
+        (u"M. Erasmus de Computació Distribuïda", "EMDC")
         ]
 
     def __init__(self):
@@ -311,7 +310,7 @@ class TriaCarrera(gtk.HBox):
         self.pack_start(self._select, expand=False)
 
     def _actualitza_carrera(self, widget=None):
-        index = self._select.get_active()-1
+        index = self._select.get_active() - 1
         if index in range(0, len(self._codiscarrera)):
             carrera = self._codiscarrera[index][1]
         else:
@@ -322,7 +321,7 @@ class TriaCarrera(gtk.HBox):
         carrera = domini.obte_carrera()
         index = [i for i, v in enumerate(self._codiscarrera) if v[1] == carrera]
         if len(index) > 0:
-            self._select.set_active(index[0]+1)
+            self._select.set_active(index[0] + 1)
         else:
             self._select.set_active(0)
 
@@ -569,15 +568,15 @@ class LlistaHoraris(gtk.TreeView):
         # Afegeix una columna buida al final
         col = gtk.TreeViewColumn("", gtk.CellRendererText())
         self.append_column(col)
-        
+
         self.connect('cursor-changed', self._horari_seleccionat_cb)
 
         # Afegir fila 'grups seleccionats'
         model = gtk.ListStore(object, bool, int, int, int, int, int)
         h = Horari()
-        it = model.append([h.grups(), False, h.hores, h.hores_mati, h.hores_tarda,
+        model.append([h.grups(), False, h.hores, h.hores_mati, h.hores_tarda,
             h.solapaments, h.fragments])
-        
+
         model.set_sort_func(0, self._cmp_horaris_cb)
         self.set_model(model)
         self._fila_grups_sel = gtk.TreeRowReference(model, (0,))
@@ -745,7 +744,6 @@ class FinestraCerca(gtk.Dialog):
         label.set_mnemonic_widget(self._min_assig)
         table.attach(label, 0, 1, 0, 1, gtk.FILL, 0)
 
-        hbox = gtk.HBox()
         self._max_solap = gtk.SpinButton()
         self._max_solap.set_range(0, 60)
         self._max_solap.set_increments(1, 1)
@@ -786,7 +784,7 @@ class FinestraCerca(gtk.Dialog):
             self._n_combinacions = self._cerca.n_combinacions()
             self._atura = False
             gobject.timeout_add(100, self._actualitza_barra_cb)
-            self._llista.actualitza(horaris = self._cerca_horaris())
+            self._llista.actualitza(horaris=self._cerca_horaris())
             self.set_response_sensitive(self.RESPONSE_ATURA, False)
             self.set_response_sensitive(self.RESPONSE_CERCA, True)
             self._min_assig.set_sensitive(True)
@@ -798,7 +796,8 @@ class FinestraCerca(gtk.Dialog):
             if gtk.events_pending():
                 gtk.main_iteration(False)
             yield h
-            if self._atura: break
+            if self._atura:
+                break
         else:
             self._atura = True
 
@@ -1112,7 +1111,7 @@ class FinestraAjuda(gtk.Dialog):
         flags = gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT
         buttons = (gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE)
         gtk.Dialog.__init__(self, "Ajuda", finestra, flags, buttons)
-        self.set_default_size(600,400)
+        self.set_default_size(600, 400)
         self.set_icon_name(gtk.STOCK_HELP)
         self.set_has_separator(False)
         tb = self._prepara_text_buffer()
@@ -1158,4 +1157,3 @@ class FinestraAjuda(gtk.Dialog):
     def mostra(self, widget=None):
         self.run()
         self.hide()
-

commit 06b33dfeaa39fd9fdbe5a9a94088ea4c7a1ca861

Fix bug introduced in 14f0ab9

src/domini.py
@@ -18,6 +18,7 @@
 
 
 import dades
+from dades import ErrorDades, ErrorOpcions
 from itertools import chain
 
 _carrera = ""